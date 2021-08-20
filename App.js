64ded8349f64a256f6c635f913edfda6b8e7a700
Sandile-Repos <sandile.developer@gmail.com>
2021-08-21 01:18:26 +0200

    stylesheets

--- App.js
+++ App.js
@@ -6,48 +6,29 @@ export default function App() {
     "Open up App.js to start working on your app!"
   );
   return (
-    <View
-      style={{
-        padding: 50,
-        flexDirection: "row",
-        width: "80%",
-        height: 300,
-        justifyContent: "space-between",
-        alignItems: "stretch",
-      }}
-    >
-      <View
-        style={{
-          backgroundColor: "red",
-          flex: 2,
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <Text>1</Text>
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput placeholder="Course Goals" style={styles.input} />
+        <Button title="ADD" />
       </View>
-      <View
-        style={{
-          backgroundColor: "blue",
-          flex: 1,
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <Text>2</Text>
-      </View>
-      <View
-        style={{
-          backgroundColor: "green",
-          flex: 2,
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <Text>3</Text>
-      </View>
+      <View />
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  screen: {
+    padding: 50,
+  },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "stretch",
+  },
+  input: {
+    width: "80%",
+    borderColor: "black",
+    borderWidth: 1,
+    padding: 10,
+  },
+});
