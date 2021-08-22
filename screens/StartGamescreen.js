bd127999ee861e099c8a85bd0e02ed14b04aeb67
Sandile-Repos <sandile.developer@gmail.com>
2021-08-22 11:00:12 +0200

    Styling Textinput

--- screens/StartGamescreen.js
+++ screens/StartGamescreen.js
@@ -1,31 +1,67 @@
-import React from "react";
-import { Button, StyleSheet, Text, TextInput, View } from "react-native";
-import { Colors } from "react-native/Libraries/NewAppScreen";
+import React, { useState } from "react";
+import {
+  Button,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableNativeFeedback,
+  TouchableWithoutFeedback,
+  View,
+  Keyboard,
+} from "react-native";
 
 import Card from "../components/Card";
+import Input from "../components/Input";
 import colors from "../constants/colors";
 
 const StartGamescreen = (props) => {
+  const [enteredValue, setEnteredValue] = useState("");
+
+  const numberInputHandler = (InputText) => {
+    setEnteredValue(InputText.replace(/[^0-9]/g, "")); //Validation to only allow numbers on input
+  };
+
   return (
-    <View style={styles.screen}>
-      <Text style={styles.title}>Start a New Game</Text>
-      <Card style={styles.inputContainer}>
-        <Text>Select a Number</Text>
-        <TextInput />
-        <View style={styles.buttonContainer}>
-          <View style={styles.button}>
-            <Button title={"Reset"} onPress={() => {}} color={colors.accent} />
+    // Keyboard is an Api(not component) from react native that is called to dismiss keybord if you press outside of input or keybord
+    <TouchableWithoutFeedback
+      onPress={() => {
+        Keyboard.dismiss();
+      }}
+    >
+      <View style={styles.screen}>
+        <Text style={styles.title}>Start a New Game</Text>
+        <Card style={styles.inputContainer}>
+          <Text>Select a Number</Text>
+          <Input
+            style={styles.input}
+            blurOnSubmit
+            autoCapitalize="none"
+            autoCorrect={false}
+            keyboardType="numeric" //number can also include period or comma - for both devices
+            // keyboardType="numer-pad" //number only, not including period or comma - ios only
+            maxLength={2}
+            onChangeText={numberInputHandler}
+            value={enteredValue}
+          />
+          <View style={styles.buttonContainer}>
+            <View style={styles.button}>
+              <Button
+                title={"Reset"}
+                onPress={() => {}}
+                color={colors.accent}
+              />
+            </View>
+            <View style={styles.button}>
+              <Button
+                title={"confirm"}
+                onPress={() => {}}
+                color={colors.primary}
+              />
+            </View>
           </View>
-          <View style={styles.button}>
-            <Button
-              title={"confirm"}
-              onPress={() => {}}
-              color={colors.primary}
-            />
-          </View>
-        </View>
-      </Card>
-    </View>
+        </Card>
+      </View>
+    </TouchableWithoutFeedback>
   );
 };
 
@@ -54,5 +90,9 @@ const styles = StyleSheet.create({
   },
   button: {
     width: 100,
+  },
+  input: {
+    width: 50,
+    textAlign: "center",
   },
 });
