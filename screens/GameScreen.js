697e60a92a5c91d1205b55e31e7074b3436210a4
Sandile-Repos <sandile.developer@gmail.com>
2021-08-24 00:40:50 +0200

    styling list items and list

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,29 +1,30 @@
 import React, { useState, useRef, useEffect } from "react";
-import {
-  Button,
-  StyleSheet,
-  Text,
-  View,
-  Alert,
-  ScrollView,
-} from "react-native";
+import { StyleSheet, Text, View, Alert, ScrollView } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 
 import Card from "../components/Card";
 import MainButton from "../components/MainButton";
 import NumberContainer from "../components/NumberContainer";
 import defaultStyles from "../constants/default-styles";
+import BodyText from "../components/BodyText";
 
 const generateRandomBetween = (min, max, exclude) => {
   min = Math.ceil(min);
   max = Math.floor(max);
   const rndNum = Math.floor(Math.random() * (max - min) + min);
-  // if (rndNum === exclude) {
-  //   return generateRandomBetween(min, max, exclude);
-  // } else {
-  return rndNum;
-  // }
+  if (rndNum === exclude) {
+    return generateRandomBetween(min, max, exclude);
+  } else {
+    return rndNum;
+  }
 };
+
+const renderListItem = (value, numOfRound) => (
+  <View key={value} style={styles.listItem}>
+    <BodyText>#{numOfRound}</BodyText>
+    <BodyText>{value}</BodyText>
+  </View>
+);
 
 const GameScreen = (props) => {
   const initialGuess = generateRandomBetween(1, 100, props.userChoice);
@@ -76,13 +77,13 @@ const GameScreen = (props) => {
           <Ionicons name="md-add" size={24} color="white" />
         </MainButton>
       </Card>
-      <ScrollView>
-        {passGuesses.map((guess) => (
-          <View key={guess}>
-            <Text>{guess}</Text>
-          </View>
-        ))}
-      </ScrollView>
+      <View style={styles.list}>
+        <ScrollView>
+          {passGuesses.map((guess, index) =>
+            renderListItem(guess, passGuesses.length - index)
+          )}
+        </ScrollView>
+      </View>
     </View>
   );
 };
@@ -101,5 +102,18 @@ const styles = StyleSheet.create({
     marginTop: 20,
     width: 400,
     maxWidth: "90%",
+  },
+  list: {
+    width: "80%",
+    flex: 1, // so list can scroll
+  },
+  listItem: {
+    borderColor: "#ccc",
+    borderWidth: 1,
+    padding: 15,
+    marginVertical: 10,
+    backgroundColor: "white",
+    flexDirection: "row",
+    justifyContent: "space-between",
   },
 });
