644554bf47ef838fabd6e29a0fb558be3a687905
Sandile-Repos <sandile.developer@gmail.com>
2021-08-25 23:18:41 +0200

    loading meals for categories

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,47 +1,38 @@
 import React from "react";
-import { View, Text, Button, StyleSheet } from "react-native";
-import { CATEGORIES } from "../data/dummy-data";
+import { render } from "react-dom";
+import { View, Text, FlatList, StyleSheet } from "react-native";
+import { CATEGORIES, MEALS } from "../data/dummy-data";
 
 const CategoryMealScreen = (props) => {
   const catId = props.navigation.getParam("categoryId");
 
-  const selectedCategory = CATEGORIES.find((cat) => cat.id === catId);
+  const displayedMeals = MEALS.filter(
+    (meal) => meal.categoryIds.indexOf(catId) >= 0
+  );
+  const renderMealItem = (itemData) => {
+    return (
+      <View>
+        <Text>{itemData.item.title}</Text>
+      </View>
+    );
+  };
+
   return (
     <View style={styles.screen}>
-      <Text>The Category Meal Screen!</Text>
-      <Text>{selectedCategory.title}</Text>
-      <View>
-        <Button
-          title="Go to Details"
-          onPress={() => {
-            props.navigation.navigate({
-              routeName: "MealDetail",
-            });
-          }}
-        />
-        <View style={{ height: 20 }}></View>
-        <Button
-          title="Go Back"
-          onPress={() => {
-            props.navigation.pop();
-          }}
-        />
-      </View>
+      <FlatList
+        data={displayedMeals}
+        renderItem={renderMealItem}
+        keyExtractor={(item, index) => item.id}
+      />
     </View>
   );
 };
 
-// selectedCategory.title is available but inside component function and cannot be accessed on navigationOptions
-// if its an object with static hard coded values
-// CategoryMealScreen.navigationOptions = {
-//   headerTitle:
-// }
-
-//But can also be a function if you need dynamic config that depends on changing data
 CategoryMealScreen.navigationOptions = (navigationData) => {
   const catId = navigationData.navigation.getParam("categoryId");
   // console.log(catId);
   const selectedCategory = CATEGORIES.find((cat) => cat.id === catId);
+
   return {
     headerTitle: selectedCategory.title,
   };
