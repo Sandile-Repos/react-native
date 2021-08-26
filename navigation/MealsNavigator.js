5f86b390cdd300ed6e0524808c4b19539a9a4349
Sandile-Repos <sandile.developer@gmail.com>
2021-08-26 14:25:16 +0200

    setting tabs and configuring icons

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -4,6 +4,7 @@ import { createStackNavigator } from "react-navigation-stack";
 import { createAppContainer } from "react-navigation";
 // import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { createBottomTabNavigator } from "react-navigation-tabs";
+import { Ionicons } from "@expo/vector-icons";
 
 import CategoriesScreen from "../screens/CategoriesScreen";
 import CategoryMealsScreen from "../screens/CategoryMealsScreen";
@@ -37,10 +38,40 @@ const MealsNavigator = createStackNavigator(
   }
 );
 
-const MealsFavTabNavigator = createBottomTabNavigator({
-  Meals: MealsNavigator, // can be single screen or stack
-  Favourite: FavouriteScreen,
-});
+const MealsFavTabNavigator = createBottomTabNavigator(
+  {
+    Meals: {
+      screen: MealsNavigator,
+      navigationOptions: {
+        tabBarIcon: (tabInfo) => {
+          return (
+            <Ionicons
+              name="ios-restaurant"
+              size={25}
+              color={tabInfo.tintcColor}
+            />
+          );
+        },
+      },
+    }, // can be single screen or stack
+    Favourite: {
+      screen: FavouriteScreen,
+      navigationOptions: {
+        tabBarLabel: "Favourites!",
+        tabBarIcon: (tabInfo) => {
+          return (
+            <Ionicons name="ios-star" size={25} color={tabInfo.tintColor} />
+          );
+        },
+      },
+    },
+  },
+  {
+    tabBarOptions: {
+      activeTintColor: Colors.accentColor,
+    },
+  }
+);
 
 // export default createAppContainer(MealsNavigator);
 export default createAppContainer(MealsFavTabNavigator);
