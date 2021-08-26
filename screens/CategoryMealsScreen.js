7218283f5ac442ff8f36d2c14d4f3a9824baf6bc
Sandile-Repos <sandile.developer@gmail.com>
2021-08-26 09:23:22 +0200

    Passing data to the meal detail screen

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -18,7 +18,14 @@ const CategoryMealScreen = (props) => {
         duration={itemData.item.duration}
         complexity={itemData.item.complexity.toUpperCase()}
         affordability={itemData.item.affordability.toUpperCase()}
-        onSelectMeal={() => {}}
+        onSelectMeal={() => {
+          props.navigation.navigate({
+            routeName: "MealDetail",
+            params: {
+              mealId: itemData.item.id,
+            },
+          });
+        }}
       />
     );
   };
