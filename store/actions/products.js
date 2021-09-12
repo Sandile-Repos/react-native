7e61912abcdb75f8d506dba4fe28e787764da8da
Sandile-Repos <sandile.developer@gmail.com>
2021-09-12 17:42:05 +0200

    loading spinner and handling errors

--- store/actions/products.js
+++ store/actions/products.js
@@ -8,36 +8,44 @@ export const SET_PRODUCTS = "SET_PRODUCTS";
 export const fetchProducts = () => {
   return async (dispatch) => {
     // any async code you want!
-    const response = await fetch(
-      "https://rn-fundamentals-f2738-default-rtdb.firebaseio.com/products.json"
-    );
-
-    const resData = await response.json();
-    //     console.log(resData);
-    //     Object {
-    //   "-MjOqpNJLEPAP5jMQwll": Object {
-    //     "description": "A very good quality white shirt",
-    //     "imageUrl": "https://cdn.pixabay.com/photo/2017/01/13/04/56/blank-1976334_960_720.png",
-    //     "price": 39.99,
-    //     "title": "A White Shirt",
-    //   },
-    // }
-    const loadedProducts = [];
-
-    for (const key in resData) {
-      loadedProducts.push(
-        new Product(
-          key,
-          "u1",
-          resData[key].title,
-          resData[key].imageUrl,
-          resData[key].description,
-          resData[key].price
-        )
+    try {
+      const response = await fetch(
+        "https://rn-fundamentals-f2738-default-rtdb.firebaseio.com/products.json"
       );
-    }
 
-    dispatch({ type: SET_PRODUCTS, products: loadedProducts });
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
+      }
+
+      const resData = await response.json();
+      //     console.log(resData);
+      //     Object {
+      //   "-MjOqpNJLEPAP5jMQwll": Object {
+      //     "description": "A very good quality white shirt",
+      //     "imageUrl": "https://cdn.pixabay.com/photo/2017/01/13/04/56/blank-1976334_960_720.png",
+      //     "price": 39.99,
+      //     "title": "A White Shirt",
+      //   },
+      // }
+      const loadedProducts = [];
+
+      for (const key in resData) {
+        loadedProducts.push(
+          new Product(
+            key,
+            "u1",
+            resData[key].title,
+            resData[key].imageUrl,
+            resData[key].description,
+            resData[key].price
+          )
+        );
+      }
+      dispatch({ type: SET_PRODUCTS, products: loadedProducts });
+    } catch (err) {
+      // send to custom analytics server
+      throw err;
+    }
   };
 };
 
