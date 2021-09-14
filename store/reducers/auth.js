dc94bfbfcbeedece927ab98a7e050d4dd17bfa02
Sandile-Repos <sandile.developer@gmail.com>
2021-09-14 21:39:52 +0200

    add logout

--- store/reducers/auth.js
+++ store/reducers/auth.js
@@ -13,16 +13,8 @@ export default (state = initialState, action) => {
         token: action.token,
         userId: action.userId,
       };
-    // case LOGIN:
-    //   return {
-    //     token: action.token,
-    //     userId: action.userId,
-    //   };
-    // case SIGNUP:
-    //   return {
-    //     token: action.token,
-    //     userId: action.userId,
-    //   };
+    case LOGOUT:
+      return initialState;
     default:
       return state;
   }
