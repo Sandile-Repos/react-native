d7ecd958cecaeedb916fc9c80cb9d2c691f7f95c
Sandile-Repos <sandile.developer@gmail.com>
2021-09-15 22:06:37 +0200

    Store Image on File System

--- store/places-actions.js
+++ store/places-actions.js
@@ -1,5 +1,23 @@
+import * as FileSystem from "expo-file-system";
+
 export const ADD_PLACE = "ADD_PLACE";
 
 export const addPlace = (title, image) => {
-  return { type: ADD_PLACE, placeData: { title: title, image: image } };
+  return async (dispatch) => {
+    const fileName = image.split("/").pop(); //split image path by / and get filename(last element)
+    const newPath = FileSystem.documentDirectory + fileName;
+
+    try {
+      await FileSystem.moveAsync({
+        //moves file from a to b
+        from: image,
+        to: newPath,
+      });
+    } catch (err) {
+      console.log(err);
+      throw err;
+    }
+
+    dispatch({ type: ADD_PLACE, placeData: { title: title, image: newPath } });
+  };
 };
